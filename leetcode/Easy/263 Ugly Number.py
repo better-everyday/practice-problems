d5839c7f838d161952728ae0f193d1280e4a6364
williampol <103951515+williampol@users.noreply.github.com>
2022-07-28 14:56:25 -0400

    Completed 263. Ugly Number
    I cheated on this one, but let's call it a test run for the repository.

---
+++ leetcode/Easy/263 Ugly Number.py
@@ -0,0 +1,34 @@
+"""
+--- Description ---
+
+An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+Given an integer n, return true if n is an ugly number.
+"""
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        if n == 1 or n == 2 or n == 3 or n == 5:
+            return True
+        if n%2 == 0:
+            n = n/2
+            return self.isUgly(n)
+
+        if n%3 == 0:
+            n = n/3
+            return self.isUgly(n)
+
+        if n%5 ==0:
+            n = n/5
+            return self.isUgly(n)
+
+        return False                                                                                                     
+
+"""
+--- Submission ---
+
+Runtime: 41 ms, faster than 74.92% of Python3 online submissions for Ugly Number.
+Memory Usage: 13.9 MB, less than 13.00% of Python3 online submissions for Ugly Number.
+"""
