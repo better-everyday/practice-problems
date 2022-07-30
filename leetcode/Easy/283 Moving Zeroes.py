6018cc0617c7e265c8c29deb6f962db236921f06
williampol <103951515+williampol@users.noreply.github.com>
2022-07-30 11:55:54 -0400

    Completed 283 Moving Zeroes
    Took a couple of different approaches but I finally got it. The final, successful approach has a time complexity of O(n), however, I found out after reading the discussions that you can use array transformations, but not reassignments. TODO use array transformations.

---
+++ leetcode/Easy/283 Moving Zeroes.py
@@ -0,0 +1,51 @@
+# type:ignore
+"""
+--- Description ---
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+"""
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        # Iterate through array and remove all zeroes, then append zeroes to end
+        # count = nums.count(0)
+        # nums = list(filter(lambda a: a != 0, nums))
+        # for i in range(count):
+        #     nums.append(0)
+        # Apparently this doesn't work because we have to do swapping
+
+        # Find first non-zero number by getting a reference from a list without zeroes and swap with the first zeri
+        # ordered = 0
+        # if nums.count(0) != 0:
+        #     while (len(nums) - nums.count(0)) != ordered:
+        #         nonzero = list(filter(lambda a: a != 0, nums))
+
+        #         first_nonzero = nums.index(nonzero[ordered])
+        #         first_zero = nums.index(0)
+
+        #         if first_nonzero > first_zero:
+        #             nums[first_zero], nums[first_nonzero] = nums[first_nonzero], nums[first_zero]
+
+        #         ordered += 1
+        # This method does not work either because the question was unclear on whether there could be duplicates in the array
+
+        # Find first non-zero iteratively
+        ordered = 0
+        if nums.count(0) != 0:
+            for i in range(len(nums)):
+                if nums[ordered] != 0 and ordered > nums.index(0):
+                    nums[nums.index(0)], nums[ordered] = nums[ordered], nums[nums.index(0)]
+                ordered += 1
+
+"""
+--- Submission ---
+
+Runtime: 7443 ms, faster than 5.01% of Python3 online submissions for Move Zeroes.
+Memory Usage: 15.4 MB, less than 96.90% of Python3 online submissions for Move Zeroes.
+"""
