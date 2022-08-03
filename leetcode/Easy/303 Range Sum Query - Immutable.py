cd8c8d15d3d1397e93fab934cae739d701845b98
williampol <103951515+williampol@users.noreply.github.com>
2022-08-03 11:31:43 -0400

    Completed 303 v2
    Seems like Prefix Sum is faster because it calculates the sums in a clever way and then the function SumRange just retrieves it. Also sum() is slow.

--- leetcode/Easy/303 Range Sum Query - Immutable.py
+++ leetcode/Easy/303 Range Sum Query - Immutable.py
@@ -14,18 +14,36 @@ Implement the NumArray class:
 """
 
 # We have to instantiate the class in this problem as well
-class NumArray:
+# class NumArray:
 
-    def __init__(self, nums: List[int]):
-        self.nums = nums
+#     def __init__(self, nums: List[int]):
+#         self.nums = nums
         
 
+#     def sumRange(self, left: int, right: int) -> int:
+#         return sum(self.nums[left:right+1])
+
+
+# The other solutions used a method called prefix sum which is way faster
+class NumArray:
+    def __init__(self, nums: List[int]):
+        self.prefixSum = [nums[0]]
+        for i in range(1, len(nums)):
+            self.prefixSum.append(self.prefixSum[i-1]+nums[i])
+    
     def sumRange(self, left: int, right: int) -> int:
-        return sum(self.nums[left:right+1])
+        if left == 0:
+            return self.prefixSum[right]
+        return self.prefixSum[right] - self.prefixSum[left-1]
 
 """
 --- Submission ---
 
+1.
 Runtime: 1163 ms, faster than 34.75% of Python3 online submissions for Range Sum Query - Immutable.
 Memory Usage: 17.5 MB, less than 69.56% of Python3 online submissions for Range Sum Query - Immutable.
+
+2.
+Runtime: 71 ms, faster than 99.80% of Python3 online submissions for Range Sum Query - Immutable.
+Memory Usage: 17.7 MB, less than 62.07% of Python3 online submissions for Range Sum Query - Immutable.
 """
