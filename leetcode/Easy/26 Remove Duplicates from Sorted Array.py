9050419b35750e7b0ced29e8912805fa867e76b0
williampol <103951515+williampol@users.noreply.github.com>
2022-08-10 18:02:11 -0400

    Updated 26
    Added first version and calculated time complexities.

--- leetcode/Easy/26 Remove Duplicates from Sorted Array.py
+++ leetcode/Easy/26 Remove Duplicates from Sorted Array.py
@@ -8,13 +8,21 @@ elements should be kept the same.
 
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
+
+        # 1
+        # for num in nums:
+        #     while nums.count(num) > 1:
+        #         nums.remove(num)
+        # return len(nums)
+
+
+        # 2
         i = 1
         for num in nums[1::]:
             if num == nums[i-1]:
                 nums.pop(i-1)
                 i -= 1
             i += 1
-            print(i, nums)
             
         return len(nums)
 
@@ -25,6 +33,10 @@ print(obj.removeDuplicates([0, 1, 1, 1, 1]))
 --- Submission ---
 
 1.
-Runtime: 5260 ms, faster than 5.02% of Python3 online submissions for Remove Duplicates from Sorted Array.
+Runtime: 5260 ms, faster than 5.02% of Python3 online submissions for Remove Duplicates from Sorted Array.      O(n^2)/O(nlogn)???
 Memory Usage: 15.6 MB, less than 16.41% of Python3 online submissions for Remove Duplicates from Sorted Array.
+
+2. 
+Runtime: 145 ms, faster than 54.28% of Python3 online submissions for Remove Duplicates from Sorted Array.      O(n)
+Memory Usage: 15.5 MB, less than 61.41% of Python3 online submissions for Remove Duplicates from Sorted Array.
 """
