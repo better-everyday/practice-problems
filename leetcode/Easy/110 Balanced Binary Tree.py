bdfd4ff9b6b42bae160e57a4d98a73bd23957fd9
williampol <103951515+williampol@users.noreply.github.com>
2022-08-18 18:38:37 -0400

    Delay 110
    I think that it would make more sense to do 111. Minimum Depth of Binary Tree first because the condition states that "a binary tree in which the left and right subtrees of every node differ in height by no more than 1," meaning that I should find the difference between the maximum and minimum depth of the tree.

---
+++ leetcode/Easy/110 Balanced Binary Tree.py
@@ -0,0 +1,17 @@
+"""
+--- Description ---
+
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+    a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+"""
+
+# Probably better to do 111. Minimum Depth of Binary Tree first because I think that if max_depth - min_depth > 1,
+# the tree is not height-balanced
+
+"""
+--- Submission ---
+
+
+"""
