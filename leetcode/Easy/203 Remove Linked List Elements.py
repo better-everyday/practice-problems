31173a493d73846f171b43f02badf1f301a21818
williampol <103951515+williampol@users.noreply.github.com>
2022-08-17 23:38:54 -0400

    Completed 203 v2
    Managed to solve the question way faster using recursion, but after looking at some of the solutions, I've found it is possible to do solve it quickly iteratively, but I don't understand the method. TODO figure out how the pointers work in the iterative method.

--- leetcode/Easy/203 Remove Linked List Elements.py
+++ leetcode/Easy/203 Remove Linked List Elements.py
@@ -12,29 +12,56 @@ class ListNode:
 
 class Solution:
     def removeElements(self, head: ListNode, val: int) -> ListNode:
-        
-        def find_tail(head):
-            current = head
-            while current.next != None:
-                current = current.next
-            return current
 
-        # Iteration
-        new_head = None
-        while head:
-            if head.val != val:
-                if new_head == None:
-                    new_head = ListNode(head.val)
+        # 1. Iteration
+        # def find_tail(head):
+        #     current = head
+        #     while current.next != None:
+        #         current = current.next
+        #     return current
+
+        # new_head = None
+        # while head:
+        #     if head.val != val:
+        #         if new_head == None:
+        #             new_head = ListNode(head.val)
+        #         else:
+        #             find_tail(new_head).next = ListNode(head.val)
+        #     head = head.next
+        # return new_head
+
+        # 2. Stitching ????
+        # def find_tail(head):
+        #     current = head
+        #     while current.val != val:
+        #         current = current.next    
+        #     return current.next
+        # new_head = None
+        # current = head
+        # while head: 
+        #     head = head.next
+        # return new_head
+
+        # 2. Recursion
+        def check(head, val):
+            if head:
+                if head.val == val:
+                    head = check(head.next, val)
                 else:
-                    find_tail(new_head).next = ListNode(head.val)
-            
-            head = head.next
-        return new_head
+                    head.next = check(head.next, val)
+
+            return head
+
+        return check(head, val)
 
 """
 --- Submission ---
 
-1. 
+1. Iteration 
 Runtime: 6875 ms, faster than 5.09% of Python3 online submissions for Remove Linked List Elements.
 Memory Usage: 19.4 MB, less than 9.02% of Python3 online submissions for Remove Linked List Elements.
+
+2. Recursion
+Runtime: 72 ms, faster than 92.78% of Python3 online submissions for Remove Linked List Elements.
+Memory Usage: 27 MB, less than 5.56% of Python3 online submissions for Remove Linked List Elements.
 """
