1eeaef388ca08c133b5aedc63a6a38ebfe4a5a39
williampol <polwilliam0@gmail.com>
2022-08-24 20:09:07 -0400

    Completed 342 v2
    Was stumped for a little bit, but then had the right idea of converting the base.

--- leetcode/Easy/342 Power of Four.py
+++ leetcode/Easy/342 Power of Four.py
@@ -11,13 +11,26 @@ from math import log
 
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
+
+        # Cheat
+        """
         if n > 0:
             if log(n, 4).is_integer():
                 return True
+        """
+        
+        # Proper
+        # 4     16      64          256     1024    36?
+        # 100   10000   1000000     100000000
+        num = bin(n)[2:]
+        return (num[0] == "1") and (num.count("0") % 2 == 0) and (num.count("1") == 1)
 
 """
 --- Submission ---
 
 Runtime: 34 ms, faster than 91.81% of Python3 online submissions for Power of Four.
+Memory Usage: 13.8 MB, less than 54.38% of Python3 online submissions for Power of Four.
+
+Runtime: 30 ms, faster than 96.09% of Python3 online submissions for Power of Four.
 Memory Usage: 13.8 MB, less than 54.38% of Python3 online submissions for Power of Four.
 """
