e41e34c4d868188992aa0752b53b1a6c6e9499ec
williampol <polwilliam0@gmail.com>
2022-09-02 14:40:51 -0400

    Completed 3 v2
    Tried recursion; faster, but not that fast and takes a lot of memory. TODO optimize recursion.

--- leetcode/Medium/3 Longest Substring Without Repeating Characters.py
+++ leetcode/Medium/3 Longest Substring Without Repeating Characters.py
@@ -33,10 +33,42 @@ class Solution:
         """
 
         # 2
+        """
+        string = ""
+        length = 0
+        s = list(s)
+        for x in range(len(s)):
+            popped = s.pop(0)
+            string += popped
+            print(string)
+            try:
+                if s.index(popped) + 1 <= len(string):
+                    if len(string) > length:
+                        length = len(string)
+                    string = ""
+            except:
+                if len(string) > length:
+                    length = len(string)
+
+        return length
+        """
+
+        # 3 Recursion (better because have to check every substring)
+        string = ""
+        length2 = 0
+        for x in s:
+            if x not in string: string += x
+            else: break
+        length = len(string)
+
+        next = s[1:]
+        if len(next) > length:
+            length2 = self.lengthOfLongestSubstring(next)
+        return max(length, length2)
 
 
 obj = Solution()
-print(obj.lengthOfLongestSubstring("pwwkew"))
+print(obj.lengthOfLongestSubstring("dvdf"))
 
 """
 --- Submission ---
@@ -44,4 +76,8 @@ print(obj.lengthOfLongestSubstring("pwwkew"))
 1. Brute Force
 Runtime: 4144 ms, faster than 5.00% of Python3 online submissions for Longest Substring Without Repeating Characters.
 Memory Usage: 14.5 MB, less than 5.33% of Python3 online submissions for Longest Substring Without Repeating Characters.
+
+3. Recursion
+Runtime: 390 ms, faster than 18.06% of Python3 online submissions for Longest Substring Without Repeating Characters.
+Memory Usage: 520.8 MB, less than 5.36% of Python3 online submissions for Longest Substring Without Repeating Characters.
 """
