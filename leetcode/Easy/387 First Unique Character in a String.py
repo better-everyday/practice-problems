8106df5e81fc8b389e0a780c7d1c69b7488715a6
williampol <polwilliam0@gmail.com>
2022-09-03 20:03:52 -0400

    Completed 387 v2
    I think more promising method if optimised.

--- leetcode/Easy/387 First Unique Character in a String.py
+++ leetcode/Easy/387 First Unique Character in a String.py
@@ -7,6 +7,8 @@ Given a string s, find the first non-repeating character in it and return its in
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         
+        # 1
+        """
         x = s
         while x:
             print(x)
@@ -17,6 +19,13 @@ class Solution:
                 return s.index(x[0])
 
         return -1
+        """
+
+        # 2
+        for i in s:
+            if s.count(0) == 1:
+                return s.index(i)
+        return -1
 
 obj = Solution()
 print(obj.firstUniqChar("aabb"))
@@ -24,6 +33,11 @@ print(obj.firstUniqChar("aabb"))
 """
 --- Submission ---
 
+1.
 Runtime: 719 ms, faster than 11.15% of Python3 online submissions for First Unique Character in a String.
 Memory Usage: 14.7 MB, less than 6.52% of Python3 online submissions for First Unique Character in a String.
+
+2.
+Runtime: 7579 ms, faster than 5.00% of Python3 online submissions for First Unique Character in a String.
+Memory Usage: 14.2 MB, less than 16.77% of Python3 online submissions for First Unique Character in a String.
 """
